e9d540ae02dfffdffb522a5e60fc99228c861ba0
leofuzita <leonardo.fuzita@aluno.ifsp.edu.br>
2023-08-16 11:19:43 -0300

    3_terceiro

---
+++ dw3backend/routes/router.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const routerApp = express.Router();
+
+// middleware that is specific to this router
+
+routerApp.use((req, res, next) => {
+    next();
+});
+
+routerApp.get("/", (req, res) => {
+    res.send("Olá mundo!");
+});
+
+//@ Rotas de Alunos
+//@ Rotas de Cursos
+// Rota Login
+
+module.exports = routerApp;
